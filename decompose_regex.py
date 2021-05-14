a6976457709079fca8738850f2fda9eb40265c46
akinorioyama <akinorioyama@users.noreply.github.com>
2021-05-14 14:52:22 +0900

    Updated with samples and docx file creation

--- decompose_regex.py
+++ decompose_regex.py
@@ -1,37 +1,72 @@
+"""
+Convert doc to docx
+
+Usage:
+  decompose_regex.py <in_file> <out_folder>
+  decompose_regex.py -h | --help
+  decompose_regex.py --version
+
+  <in_file>: filename of the doc file to be converted
+  <out_folder>: output folder for the split files and
+    zipped file named (new.docx)
+
+Examples:
+  decompose_regex.py examples/sample.doc out
+    "sample.doc" in the current folder is converted to a docx file
+    in the "out" folder under the name of "new.docx". The associated
+    intermediate files such as "[Content_Types].xml" are also stored
+    in the "out" folder.
+  decompose_regex.py examples/sample_broken.doc out_broken
+    "new.docx" will be created in "out_broken" folder. However, this
+    file is corrupted with an inconsistent relationship id, which is
+    changed from "rId4" to "rId40" in Relationships package.
+
+    A file, "Error in the sample file sample_broken.doc.jpg", under
+     examples folder shows how the inconsistency will be indicated
+     in "Open XML SDK 2.5 Productivity Tool".
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+"""
 import xml.etree.ElementTree as ET
 import os
 import re
 from xml.etree import ElementTree
+import base64
+import zipfile
+from docopt import docopt
 
 def register_all_namespaces(filename):
     namespaces = dict([node for _, node in ET.iterparse(filename, events=['start-ns'])])
     for ns in namespaces:
-        # if ( ns == "w14" or ns == "w15" or ns == "w16se" or ns == "w16cid" or
-        #     ns == "w16" or ns == "w16cex" or ns == "wp14"):
-        #     print("registering (skipped?):", ns)
-        # else:
-        #     print("registering:", ns)
         try:
             ET.register_namespace(ns, namespaces[ns])
         except ValueError as e:
             print(e)
-xml_original_filename = "a.xml"
-xml_original_filename = "D:/file_error.DOC.xml"
+
 if __name__ == '__main__':
+
+    arguments = docopt(__doc__, version="0.1")
+
+    in_file = arguments["<in_file>"]
+    out_folder = arguments["<out_folder>"]
+    xml_original_filename = in_file
+    file_to_create = 'new.docx'
+
     with open(xml_original_filename, "r",encoding="utf-8") as f:
         xml = f.read()
     pattern = re.compile(r'<pkg:part.*?>.*?</pkg:part>', re.MULTILINE | re.DOTALL)
     result = pattern.finditer( xml)
-    #print(result)
-    # <callable_iterator object at 0x10b0efa90>
 
-    # print(type(result))
-    # <class 'callable_iterator'>
     list_content_types = []
+
+    print("Step: reading packages")
+
     for m in result:
-        print(m.group()[0:50])
+        print("-->",m.group()[0:50])
         result_name = re.finditer(r'pkg:name=\".*?\"',m.group())
-        filepath = "./outxx"
+        filepath = out_folder
         for name in result_name:
             filename = name.group()[10:-1]
         filepath = filepath + filename
@@ -45,7 +80,7 @@ if __name__ == '__main__':
         result_contentType = re.findall(r'pkg:contentType=\"(.*?)\"',m.group())
         for ct in result_contentType:
             content_type_detail = ct
-            print(content_type_detail,filename)
+            print("---->",content_type_detail,filename)
             xml_contentType = content_type_detail = ct
 
 
@@ -73,10 +108,20 @@ if __name__ == '__main__':
                 list_content_types.append([xml_name,xml_contentType])
                 f.write(xml.encode('utf-8'))
 
-    with open("./outxx/[Content_Types].xml", 'w') as f:
+        pattern = re.compile(r'<pkg:binaryData>(.*?)</pkg:binaryData>', re.MULTILINE | re.DOTALL)
+        result_xml = pattern.findall(m.group())
+        for xml in result_xml:
+            with open(filepath, 'wb') as f:
+                f.write(base64.urlsafe_b64decode(xml.encode('UTF-8')))
+
+    #TODO: contentType is not exhaustive. Newly identified contentType has to be added
+    # to avoid inconsistency in Word file
+
+    with open(os.path.join(out_folder,"[Content_Types].xml"), 'w') as f:
         f.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
         f.write('<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">')
         f.write('<Default Extension="png" ContentType="image/png"/>')
+        f.write('<Default Extension="jpeg" ContentType="image/jpeg"/>')
         f.write('<Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>')
         f.write('<Default Extension="xml" ContentType="application/xml"/>')
         for content_type in list_content_types:
@@ -85,3 +130,17 @@ if __name__ == '__main__':
                 f.write('<Override PartName="' + content_type[0] +
                         '" ContentType="' + content_type[1] + '"/>')
         f.write('</Types>')
+
+    print("Step: zipping files")
+    walk_result = os.walk(out_folder)
+    with zipfile.ZipFile(os.path.join(out_folder,file_to_create), 'w', compression=zipfile.ZIP_STORED) as new_zip:
+        for dirname, subdirs, files in walk_result:
+            for filename in files:
+                if filename == file_to_create:
+                    continue
+                dir_without_root = dirname.replace(out_folder,"")
+                print("-->",dir_without_root, filename)
+                new_zip.write(os.path.join(dirname, filename),os.path.join(dir_without_root,filename))
+
+    print("\r\nCompleted.\n\r-->File \"{filename}\" is created under the folder \"{folder}\""
+          " from the input file \"{in_file}\".".format(filename=file_to_create,folder=out_folder,in_file=in_file))
